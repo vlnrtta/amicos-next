aed4674a8477c90c8ffeef98a5c292ec1fbedc1f
vlnrtta <vlnrtta@gmail.com>
2024-06-11 11:50:32 -0300

    modificacion

--- src/app/pages/ubicacion/page.js
+++ src/app/pages/ubicacion/page.js
@@ -1,12 +1,12 @@
 export default function Ubicacion() {
     return(
-        <div>
+        <div className="">
             <div id="ubicacion" className="mt-32 mb-12 w-[657px] sm:w-full px-12">
                 <p className="text-xl font-semibold text-green-700">Ubicación</p>
                 <hr className="border-green-600" />
             </div>
-            <div className="w-full flex justify-center mt-20">
-                <iframe className="shadow-2xl shadow-gray-400 rounded-xl" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3404.673727231047!2d-64.19061722526124!3d-31.423114296574987!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9432a285ed9d40ff%3A0x90f76669844263d3!2sSan%20Luis%2071%2C%20X5000BAA%20C%C3%B3rdoba!5e0!3m2!1ses-419!2sar!4v1714326398044!5m2!1ses-419!2sar" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <div className="flex justify-center mt-20 w-[550px] sm:w-full">
+                <iframe className="shadow-md shadow-gray-300 rounded-xl sm:h-[450px] h-[350px] sm:w-[600px] w-[400px]" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3404.673727231047!2d-64.19061722526124!3d-31.423114296574987!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9432a285ed9d40ff%3A0x90f76669844263d3!2sSan%20Luis%2071%2C%20X5000BAA%20C%C3%B3rdoba!5e0!3m2!1ses-419!2sar!4v1714326398044!5m2!1ses-419!2sar"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
     );
--- src/components/footer/page.js
+++ src/components/footer/page.js
@@ -20,28 +20,32 @@ export default function Footer() {
                             <a href="#" className="hover:opacity-80 me-4 md:me-6 text-gray-800">Nosotros</a>
                         </li>
                         <li>
-                            <a href="https://www.instagram.com/amicos_mutual/" className="hover:opacity-80 me-4 md:me-6 flex align-bottom">
+                            <a href="https://www.instagram.com/amicos_mutual/" className="text-gray-800 hover:opacity-80 me-4 md:me-6 flex align-bottom">
+                            Instagram
                             </a>
 
                         </li>
                         <li>
-                            <a href="https://mail.google.com/mail/u/0/#inbox?compose=jrjtXDxqxtnRDWvxvjCRskvHxBBbtQrFZxkZvxVnvZHJcMgnTFxqsncsfpkgWVdjxkgkTvKg" className="hover:opacity-80">
+                            <a href="https://mail.google.com/mail/u/0/#inbox?compose=jrjtXDxqxtnRDWvxvjCRskvHxBBbtQrFZxkZvxVnvZHJcMgnTFxqsncsfpkgWVdjxkgkTvKg" className="text-gray-800 hover:opacity-80">
+                            Gmail
                             </a>
                         </li>
                     </ul>
                 </div>
             </div>
+            
             <hr className="mb-2 border-gray-400 w-[550px] sm:w-full mx-auto" />
-            <div className="grid grid-cols-2 mx-auto w-[550px]">
+            
+            <div className="grid grid-cols-2 mx-auto w-[550px] sm:w-full">
                 <div className="text-gray-700">
                     <p>Asociación Mutual Intercooperativa Solidaria. <br></br>
                         C.U.I.T. 30-68761473-5 <br></br>
                         Mat. I.N.A.C.Y.M. Nº 680 <br></br>
                         N° de Inscripción RNEMP: 9999 | Superintendencia de Servicios de Salud.</p>
                 </div>
-                <div className="text-gray-700 inline-flex pl-12 sm:pl-0 justify-end sm:gap-2">
+                <div className="text-gray-700 inline-flex ml-12 sm:pl-0 justify-end gap-2">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.75" className="icon icon-tabler icons-tabler-outline icon-tabler-map-pin" viewBox="0 0 24 24"><path stroke="none" d="M0 0h24v24H0z"/><path d="M9 11a3 3 0 1 0 6 0 3 3 0 0 0-6 0"/><path d="M17.657 16.657 13.414 20.9a2 2 0 0 1-2.827 0l-4.244-4.243a8 8 0 1 1 11.314 0z"/></svg>
-                    <h5>San Luis 71 - Nueva Cordoba - Cordoba.</h5>
+                    <h5 className="justify-end">San Luis 71 - Nueva Cordoba - Cordoba.</h5>
                 </div>
             </div>
         </div>
