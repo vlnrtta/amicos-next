ec5483943d02a05d2550690ee7d56a0de5557887
vlnrtta <vlnrtta@gmail.com>
2024-06-11 09:09:27 -0300

    modificacion

--- src/app/pages/contactos/page.js
+++ src/app/pages/contactos/page.js
@@ -4,7 +4,7 @@ export default function Contactos() {
 
         <div>
             <p
-                className="w-[327.5px] sm:w-full inline-flex p-4 bg-white-100 border-r-2 border-red-500 hover:text-gray-900">
+                className="w-[327.5px] text-center sm:mx sm:w-full inline-flex p-4 bg-white-100 border-r-2 border-red-500 hover:text-gray-900">
                 <svg xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="1.5"  stroke-linecap="round"  stroke-linejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-phone"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" /></svg>
                 0351-4217152
             </p>
