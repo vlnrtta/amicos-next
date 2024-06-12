413c0253902bb245df66a55b6aa4ff22a4cde59b
vlnrtta <vlnrtta@gmail.com>
2024-06-12 12:10:34 -0300

    modificacion

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,16 +2,20 @@ import { Onest } from "next/font/google";
 import "./globals.css";
 import HeaderHome from "@/src/components/headerHome/page";
 import Footer from "../components/footer/page";
+import { Metadata } from "next";
 
 const onest = Onest({ subsets: ["latin"] });
 
 export const metadata = {
   title: "AMICOS - Asociación Mututal Intercooperativa Solidaria",
-};
+  lang:'es',
+  };
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
+      <link rel="icon" href="favicon.svg"></link> 
       <body className={onest.className}>
         <HeaderHome/>
         {children}
