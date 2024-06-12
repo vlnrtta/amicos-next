80e8bc86014072dc57c5f1cae128a626ca0f4408
vlnrtta <vlnrtta@gmail.com>
2024-06-12 10:56:38 -0300

    modificacion

--- src/app/nosotros/page.js
+++ src/app/nosotros/page.js
@@ -3,29 +3,29 @@ import Background from "@/src/components/background/page";
 export default function Nosotros() {
     return(
         <div>
-            <div className="grid grid-cols-2 mt-24 items-center">
+            <div className="grid grid-cols-2 mt-24 items-center sm:w-[1120px] mx-auto">
 
-                <div >
-                    <img className="rounded-xl ml-36 justify-end shadow-md h-[350px] w-[450px] shadow-gray-500" src="https://firebasestorage.googleapis.com/v0/b/amicosimagenes.appspot.com/o/foto.jpg?alt=media&token=bda9dc0b-9a5c-4497-bb8b-f23ca9545c0c" alt=""></img>
+                <div className="pl-24">
+                    <img className="rounded-xl shadow-md sm:h-[350px] sm:w-[450px] shadow-gray-500" src="https://firebasestorage.googleapis.com/v0/b/amicosimagenes.appspot.com/o/foto.jpg?alt=media&token=bda9dc0b-9a5c-4497-bb8b-f23ca9545c0c" alt=""></img>
                 </div>
 
-                <div className="pl-44">
-                    <svg className="h-64 w-64" xmlns="http://www.w3.org/2000/svg" width="70.667" height="60" version="1.0" viewBox="0 0 53 45"><g fill="#00913f"><path d="M12.7 2.2c-1.5 1.2-1.1 2 3.9 8.3 3.1 3.8 6 7.4 6.4 7.8 1.3 1.4 4.9-1.1 4.4-3-.3-1.1 1.3-3.9 4-7.2 2.5-3 4.2-5.9 3.9-6.4-.6-.9-9.2 8-9.3 9.5-.1 2-2 .4-5.6-4.7-4.1-5.8-5-6.3-7.7-4.3z"/><path d="M23.4 2.5c-1.8 1.3-1.8 1.5-.1 3.2 1.6 1.7 1.8 1.7 3.7-.2 1.9-2 1.9-2.1.1-3.2-1.5-.9-2.3-.9-3.7.2zM42 8.5c-.7.9-4.1 3.6-7.6 6.2-5.8 4.2-6.2 4.7-5.2 6.9 1.2 2.6 2.3 3 3.3 1.4.4-.6 3.3 1.1 7 4 3.5 2.8 6.6 4.6 6.9 4.1.4-.5-1.9-2.9-4.9-5.3-3-2.3-5.5-4.6-5.5-5 0-.4 2.5-2.6 5.5-4.8 5.1-3.8 5.4-4.2 4.3-6.5-1.3-2.9-2.1-3.1-3.8-1zM5 12.7c0 .5 1.5 1.8 3.4 3.1 1.8 1.2 4.3 3.3 5.5 4.6l2.3 2.4-5.6 3.9c-5.8 4-6.3 4.9-4.4 7.6 1.2 1.6 2.1 1.2 9.5-4.5 7.9-6 8.1-6.2 6.6-8.5-1.1-1.7-1.9-2.1-2.8-1.3-.9.8-2.7-.2-6.8-3.5C7.4 12.3 5 11 5 12.7z"/><path d="M42.2 19.1c-1 1.6-1 2.4.2 4.1l1.5 2.1 1.2-2.7c1-2.2 1-2.9-.2-4.1-1.3-1.3-1.6-1.2-2.7.6zM6.1 20.9c-1.6 2.8 1.4 5.1 3.6 2.8 1.3-1.3 1.4-1.9.3-3.1-1.7-2.1-2.7-2-3.9.3zM25.2 25.3c-1.7 1-1.9 1.6-1.1 2.4.9.9.1 2.6-3.4 6.9-2.6 3.1-4.7 6-4.7 6.5 0 2 1.7.6 5.9-4.6 2.4-3 4.7-5.5 5-5.5.4 0 1.4 1.2 2.3 2.7 1 1.6 2.8 4.1 4.1 5.8 2.3 2.7 2.7 2.9 5 1.6l2.5-1.3-6-7.7c-3.3-4.2-6.3-7.7-6.7-7.8-.3-.1-1.6.3-2.9 1z"/><path d="M24.8 37.4c-2.3 1.7-2.2 2.3 0 3.5 1.2.6 2.3.5 3.5-.3 1.6-1.2 1.6-1.4-.1-2.9-1.4-1.3-2-1.3-3.4-.3z"/></g></svg>
+                <div className="flex justify-end px-24">
+                    <svg className="h-64 w-64 flex justify-end" xmlns="http://www.w3.org/2000/svg" width="70.667" height="60" version="1.0" viewBox="0 0 53 45"><g fill="#00913f"><path d="M12.7 2.2c-1.5 1.2-1.1 2 3.9 8.3 3.1 3.8 6 7.4 6.4 7.8 1.3 1.4 4.9-1.1 4.4-3-.3-1.1 1.3-3.9 4-7.2 2.5-3 4.2-5.9 3.9-6.4-.6-.9-9.2 8-9.3 9.5-.1 2-2 .4-5.6-4.7-4.1-5.8-5-6.3-7.7-4.3z"/><path d="M23.4 2.5c-1.8 1.3-1.8 1.5-.1 3.2 1.6 1.7 1.8 1.7 3.7-.2 1.9-2 1.9-2.1.1-3.2-1.5-.9-2.3-.9-3.7.2zM42 8.5c-.7.9-4.1 3.6-7.6 6.2-5.8 4.2-6.2 4.7-5.2 6.9 1.2 2.6 2.3 3 3.3 1.4.4-.6 3.3 1.1 7 4 3.5 2.8 6.6 4.6 6.9 4.1.4-.5-1.9-2.9-4.9-5.3-3-2.3-5.5-4.6-5.5-5 0-.4 2.5-2.6 5.5-4.8 5.1-3.8 5.4-4.2 4.3-6.5-1.3-2.9-2.1-3.1-3.8-1zM5 12.7c0 .5 1.5 1.8 3.4 3.1 1.8 1.2 4.3 3.3 5.5 4.6l2.3 2.4-5.6 3.9c-5.8 4-6.3 4.9-4.4 7.6 1.2 1.6 2.1 1.2 9.5-4.5 7.9-6 8.1-6.2 6.6-8.5-1.1-1.7-1.9-2.1-2.8-1.3-.9.8-2.7-.2-6.8-3.5C7.4 12.3 5 11 5 12.7z"/><path d="M42.2 19.1c-1 1.6-1 2.4.2 4.1l1.5 2.1 1.2-2.7c1-2.2 1-2.9-.2-4.1-1.3-1.3-1.6-1.2-2.7.6zM6.1 20.9c-1.6 2.8 1.4 5.1 3.6 2.8 1.3-1.3 1.4-1.9.3-3.1-1.7-2.1-2.7-2-3.9.3zM25.2 25.3c-1.7 1-1.9 1.6-1.1 2.4.9.9.1 2.6-3.4 6.9-2.6 3.1-4.7 6-4.7 6.5 0 2 1.7.6 5.9-4.6 2.4-3 4.7-5.5 5-5.5.4 0 1.4 1.2 2.3 2.7 1 1.6 2.8 4.1 4.1 5.8 2.3 2.7 2.7 2.9 5 1.6l2.5-1.3-6-7.7c-3.3-4.2-6.3-7.7-6.7-7.8-.3-.1-1.6.3-2.9 1z"/><path d="M24.8 37.4c-2.3 1.7-2.2 2.3 0 3.5 1.2.6 2.3.5 3.5-.3 1.6-1.2 1.6-1.4-.1-2.9-1.4-1.3-2-1.3-3.4-.3z"/></g></svg>
                 </div>
 
             </div>
             
-            <div className="w-[657px] sm:w-[1120px] sm:mx-auto mt-24 p-4 sm:p-8 px-8 rounded-xl border border-green-700 shadow-lg shadow-gray-400 h-full">
+            <div className="mx-4 w-[657px] sm:w-[1120px] sm:mx-auto mt-24 p-4 sm:p-8 px-8 rounded-xl border border-green-700 shadow-lg shadow-gray-400 h-full">
                 <h6 className="mb-1 text-4xl font-bold tracking-tight text-green-700 text-pretty" >Historia</h6>
                 <p className="mb-3 text-xl text-gray-800 leading-relaxed" >A fines del año 1995 representantes de algunas Cooperativas de la Provincia de Córdoba, se auto convocaron con el objeto de analizar la posibilidad de crear una Mutualidad que se dedicara especialmente a atender los problemas de salud de la población del interior, pues se había constatado el requerimiento permanente y sostenido de este tipo de atención ante el decaimiento prestacional de los Hospitales Públicos, las Obras Sociales y Empresas Pre-Pagas existentes El 18 de Diciembre de 1995, se realizó la Asamblea Constitutiva de AMICOS, Entidad a la cual adhirieron inicialmente un grupo de doce (12) Cooperativas necesarias para cumplimentar las prescripciones legales de Fundación, y otras Entidades interesadas en los objetivos, dieron su apoyo, para posteriormente adherir a esta Mutual Intercooperativa. Con fecha 7 de Octubre de 1996, por Resolución N° 953, el INACyM inscribió en el Registro Nacional de Mutualidades a AMICOS, reconociéndola y autorizándola a funcionar como Asociación Mutual, bajo la Matricula N° 680 (Córdoba). Mientras se formalizaban estas inscripciones y se protocolizaban sus Estatutos, el Consejo Directivo de AMICOS, con el aporte de distintas Empresas y Profesionales, realizó las acciones necesarias para concretar los objetivos fundacionales, en una primera fase. Se elaboraron Planes de Salud con el objeto de satisfacer las necesidades detectadas, poniendo énfasis en ofrecer a los asociados prestaciones de calidad, sin pago de plus, con mínimos coseguros y carencias. En estos Planes no existe la “letra chica” que tanto perjudica a los asociados, sobre todo en momentos cruciales. Se realizó un estudio de Planeamiento Estratégico, con el objeto de concientizar y clarificar las prioridades del grupo de trabajo, en un mercado sumamente competitivo, flexible y en permanente transformación.</p>
             </div>
 
-            <div className="w-[657px] sm:w-[1120px] sm:mx-auto mt-24 p-4 sm:p-8 px-8 rounded-xl border border-green-700 shadow-lg shadow-gray-400 h-full">
+            <div className="mx-4 w-[657px] sm:w-[1120px] sm:mx-auto mt-24 p-4 sm:p-8 px-8 rounded-xl border border-green-700 shadow-lg shadow-gray-400 h-full">
                 <h6 className="mb-1 text-4xl font-bold tracking-tight text-green-700 text-pretty" >Nuestro objetivo</h6>
                 <p className="mb-3 text-xl text-gray-800 leading-relaxed" >La Aspiración de AMICOS es otorgar completa protección en la poblaciones del interior de la provincia de Córdoba, poniendo en práctica los más nobles principios solidarios, por encima de cualquier interés corporativo, federativo, regional, personal o de cualquier otra índole.</p>
             </div>
             
-            <div className="w-[657px] sm:w-[1120px] sm:mx-auto mt-24 p-4 sm:p-8 px-8 rounded-xl border border-green-700 shadow-lg shadow-gray-400 h-full">
+            <div className="mx-4 w-[657px] sm:w-[1120px] sm:mx-auto mt-24 p-4 sm:p-8 px-8 rounded-xl border border-green-700 shadow-lg shadow-gray-400 h-full">
                 <h6 className="mb-1 text-4xl font-bold tracking-tight text-green-700 text-pretty" >Comisión directiva y junta fiscalizadora</h6>
                 <ul className="mb-3 text-xl text-gray-800 leading-relaxed" >
                     <li>● Antimo N. VELAZQUEZ <b>Presidente</b> DNI 10.323.417 - LA PARA</li>
@@ -49,7 +49,7 @@ export default function Nosotros() {
                 </ul>
             </div>
 
-            <div className="w-[657px] sm:w-[1120px] sm:mx-auto mt-24 p-4 sm:p-8 px-8 rounded-xl border border-green-700 shadow-lg shadow-gray-400 h-full">
+            <div className="mx-4 w-[657px] sm:w-[1120px] sm:mx-auto mt-24 p-4 sm:p-8 px-8 rounded-xl border border-green-700 shadow-lg shadow-gray-400 h-full">
                 <h6 className="mb-1 text-4xl font-bold tracking-tight text-green-700 text-pretty" >Staff</h6>
                 <ul className="mb-3 text-xl text-gray-800 leading-relaxed" >
                     <li>● COORDINADOR GENERAL - Franco Amaya</li>
